SuperiorOS-Devices/device_xiaomi_daisy

daisy: Switch drm HAL To version 1.3
Bug: 168066487
Test: Manual

Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
Change-Id: I4eb09fd2403d9366ff775aee0afefef6783f3d41
Signed-off-by: TogoFire <italomellopereira@gmail.com>

device.mk
@@ -216,7 +216,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.3-service.clearkey \
+    android.hardware.drm@1.3-service.widevine
 
 # Spectrum
 PRODUCT_PACKAGES += \

hestayn/device_xiaomi_daisy_r

Signed-off-by: lupesoltec <dev@lupesoltec.com.br>

device.mk
@@ -226,7 +226,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.3-service.clearkey
 
 # Ebtables
 PRODUCT_PACKAGES += \