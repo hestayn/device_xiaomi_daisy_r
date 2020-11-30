dec8410aaef9cfdc26cb3c4af97336f07923e686
Hellyx <60854821+hestayn@users.noreply.github.com>
2020-11-30 11:24:28 +0300

    daisy: prop: use Skia's Vulkan renderer

--- prop.mk
+++ prop.mk
@@ -113,7 +113,7 @@ sdm.debug.disable_skip_validate=1 \
 vendor.display.disable_skip_validate=1 \
 vendor.display.enable_default_color_mode=0 \
 vendor.gralloc.enable_fb_ubwc=1 \
-debug.hwui.renderer=opengl
+debug.hwui.renderer=skiavk
 
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
